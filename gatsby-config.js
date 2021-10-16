d58de93bc352b850b8331ca1a54d632f58886717
DAVID ARIEL LARRONDA <dlard707@yahoo.com.ar>
2021-10-16 13:13:44 -0300

    cambio cor de fundo

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,7 +69,7 @@ module.exports = {
         description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
         start_url: `/`,
         background_color: `#fff`,
-        theme_color: `#6B46C1`,
+        theme_color: `#171738`,
         display: `standalone`,
         icons: [
           {
